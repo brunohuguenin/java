938976d9b10cca91af65c41bb32ddaf709d604af
brunohuguenin <brunohugueninn@gmail.com>
2024-11-18 13:58:00 -0300

    Aula 11
    Variáveis - tipos primitivos

---
+++ JAVA-CURSO/src/aulas/aula11/main/VariaveisBoolean.java
@@ -0,0 +1,10 @@
+package aulas.aula11.main;
+
+public class VariaveisBoolean {
+    public static void main(String[] args) {
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        System.out.println("Java é a melhor linguagem de programação ? " + verdadeiro);
+    }
+}
---
+++ JAVA-CURSO/src/aulas/aula11/main/VariaveisPontoFlutante.java
@@ -0,0 +1,11 @@
+package aulas.aula11.main;
+
+public class VariaveisPontoFlutante {
+    public static void main(String[] args) {
+        double valorPassagem = 4.30;
+        float valorTomate = 3.95f;
+
+        System.out.println("Valor da passagem = R$" + valorPassagem);
+        System.out.println("Valor do tomate = R$" + valorTomate);
+    }
+}
