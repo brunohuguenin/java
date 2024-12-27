6f30b6d1e6babfb98e7d73cbfca2715f67fd50e0
brunohuguenin <brunohugueninn@gmail.com>
2024-12-27 17:55:06 -0300

    exercício 05 da aula14/15

---
+++ JAVA-CURSO/src/exercicios/exAulas14e15/ex05.java
@@ -0,0 +1,31 @@
+package exercicios.exAulas14e15;
+
+import java.util.Scanner;
+
+public class ex05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insira o nome do aluno e em seguida as notas do bimestre");
+        System.out.print("NOME: ");
+        String nome = scan.next();
+
+        System.out.print("1ª NOTA: ");
+        double nota1 = scan.nextDouble();
+
+        System.out.print("2ª NOTA: ");
+        double nota2 = scan.nextDouble();
+
+        double media = (nota1 + nota2) / 2;
+
+        if (media == 10) {
+            System.out.printf("O aluno %s obteve a seguinte média %.2f e por isso está APROVADO COM DISTINÇÃO!!", nome, media);
+        } else if (media >= 7 && media < 10) {
+            System.out.printf("O aluno %s obteve a seguinte média %.2f e por isso está aprovado.", nome, media);
+        } else if (media < 7) {
+            System.out.printf("O aluno %s obteve a seguinte média %.2f e por isso está reprovado.", nome, media);
+        } else {
+            System.out.println("Nota Inválida");
+        }
+    }
+}
