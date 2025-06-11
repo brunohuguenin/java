d8b872d7f2fb6d2a88a2230a65a7e2bcd26c42f5
brunohuguenin <brunohugueninn@gmail.com>
2025-06-11 14:49:25 -0300

    Aula 34
    Teoria sobre métodos e variáveis estáticas

---
+++ JAVA-CURSO/src/aulas/aula34/TesteCalculadora.java
@@ -0,0 +1,12 @@
+package aulas.aula34;
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        //Calculadora cal = new Calculadora();
+
+        //int soma = cal.soma(35, 67);
+
+        System.out.println(Calculadora.soma(65, 37));
+
+    }
+}
