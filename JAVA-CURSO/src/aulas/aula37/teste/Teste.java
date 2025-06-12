0c238914158e59cc674263adb0c59182f9eff7ff
brunohuguenin <brunohugueninn@gmail.com>
2025-06-12 13:16:23 -0300

    Aula 37
    Teoria sobre herança

---
+++ JAVA-CURSO/src/aulas/aula37/teste/Teste.java
@@ -0,0 +1,25 @@
+package aulas.aula37.teste;
+
+import aulas.aula37.dominio.Aluno;
+import aulas.aula37.dominio.Pessoa;
+import aulas.aula37.dominio.Professor;
+
+public class Teste {
+    public static void main(String[] args) {
+//        Aluno aluno = new Aluno();
+//
+//        Professor professor = new Professor();
+//
+//        Pessoa pessoa = new Pessoa();
+//
+//        aluno.setNome("Obito");
+//
+//        aluno.setTelefoneCelular("21 979814-0458");
+
+        Pessoa aluno = new Aluno();
+        //aluno.getCpf();
+
+        Pessoa professor = new Professor();
+        //professor.setNome();
+    }
+}
