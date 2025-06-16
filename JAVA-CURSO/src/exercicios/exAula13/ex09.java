93d9f8f39fba22f66402fbc71534dab88d85b6d1
brunohuguenin <brunohugueninn@gmail.com>
2025-06-16 17:52:30 -0300

    Exercício 09
    exercício

---
+++ JAVA-CURSO/src/exercicios/exAula13/ex09.java
@@ -0,0 +1,16 @@
+package exercicios.exAula13;
+
+import java.util.Scanner;
+
+public class ex09 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com a temperatura em Farenheit ºF: ");
+        double temFarenHeit = scan.nextDouble();
+
+        double temCelsius = 5 * ((temFarenHeit - 32) / 9);
+
+        System.out.printf("A temperatura de %.2fºF é equivalente a %.2fºC", temFarenHeit, temCelsius);
+    }
+}
