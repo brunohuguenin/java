9860bef2b41d07e4905906a85a3bb51750ae1774
brunohuguenin <brunohugueninn@gmail.com>
2025-06-23 13:57:28 -0300

    Aula 65
    Teoria sobre annotations

---
+++ JAVA-CURSO/src/aulas/aula65/Teste.java
@@ -0,0 +1,12 @@
+package aulas.aula65;
+
+@InformacaoAula(
+        autor = "Loiane",
+        aulaNumero = 65,
+        blog = "loiane.com"
+)
+public class Teste {
+    public static void main(String[] args) {
+        
+    }
+}
