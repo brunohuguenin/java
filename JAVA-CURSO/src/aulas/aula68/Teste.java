68f41cb6572ac81b1bc9bd4adb55167f216e04eb
brunohuguenin <brunohugueninn@gmail.com>
2025-06-23 21:42:17 -0300

    Aula 68
    interface runnable

---
+++ JAVA-CURSO/src/aulas/aula68/Teste.java
@@ -0,0 +1,13 @@
+package aulas.aula68;
+
+public class Teste {
+    public static void main(String[] args) {
+        MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 500);
+        //Thread t1 = new Thread(thread1);
+        //t1.start();
+
+        MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 900);
+
+        MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 1200);
+    }
+}
