addf62fc838c3cc7eadc51ecba820efedae10910
brunohuguenin <brunohugueninn@gmail.com>
2025-06-25 16:01:26 -0300

    Aula 79
    Teoria sobre String: fazendo buscas (indexOf, lastIndexOf, contains)

---
+++ JAVA-CURSO/src/aulas/aula79/Aula79.java
@@ -0,0 +1,22 @@
+package aulas.aula79;
+
+public class Aula79 {
+    public static void main(String[] args) {
+
+        String banana = "banana";
+        String ana = "ana";
+        String abacaxi = "abacaxi";
+
+        System.out.println(banana.indexOf("c"));
+        System.out.println(banana.indexOf("n"));
+        System.out.println(abacaxi.indexOf("xi"));
+
+
+        System.out.println(abacaxi.lastIndexOf("a"));
+
+
+        System.out.println(abacaxi.contains("ca"));
+        System.out.println(abacaxi.contains("ana"));
+
+    }
+}
