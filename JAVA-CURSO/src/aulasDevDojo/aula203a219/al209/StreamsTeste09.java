0c92a54d9d34517f390c28eac02be78a76b84e0d
brunohuguenin <brunohugueninn@gmail.com>
2025-07-29 09:29:16 -0300

    Aula 210
    Teoria sobre Reduce

---
+++ JAVA-CURSO/src/aulasDevDojo/aula203a219/al209/StreamsTeste09.java
@@ -0,0 +1,33 @@
+package aulasDevDojo.aula203a219.al209;
+
+import aulasDevDojo.aula203a219.al205.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamsTeste09 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no Life", 2.99),
+            new LightNovel("Fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Kumo Desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum)
+                .ifPresent(System.out::println);
+
+        double sum = lightNovels.stream()
+                .mapToDouble(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .sum();
+        System.out.println(sum);
+    }
+}
