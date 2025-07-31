a9c06483d2aabbe3683416a7034f8b3439d149fe
brunohuguenin <brunohugueninn@gmail.com>
2025-07-31 13:50:38 -0300

    Código de teste da revisão
    Revisão sobre Comparable<> e Comparator<>

---
+++ JAVA-CURSO/src/Testes/collection/F_sortingList/Carros.java
@@ -0,0 +1,64 @@
+package Testes.collection.F_sortingList;
+
+import java.util.Objects;
+
+public class Carros implements Comparable<Carros>{
+    private Long id;
+    private String Marca;
+    private double preco;
+
+    public Carros(Long id, String marca, double preco) {
+        this.id = id;
+        Marca = marca;
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carros carros = (Carros) o;
+        return Double.compare(preco, carros.preco) == 0 && Objects.equals(Marca, carros.Marca);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Marca, preco);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Carros{" +
+                "id=" + id +
+                ", Marca='" + Marca + '\'' +
+                ", preco = R$" + preco +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Carros outro) {
+        return Double.compare(preco, outro.getPreco());
+    }
+
+
+}
