cf5202bb11b341c6555998e0af91f7203e5883a8
brunohuguenin <brunohugueninn@gmail.com>
2025-08-02 22:42:50 -0300

    Código de revisão
    Revisão sobre generics

---
+++ JAVA-CURSO/src/Testes/collection/M_generics/Caixa.java
@@ -0,0 +1,13 @@
+package Testes.collection.M_generics;
+
+public class Caixa<T> {
+    private T conteudo;
+
+    public void guardar(T item) {
+        this.conteudo = item;
+    }
+
+    public T abrir() {
+        return conteudo;
+    }
+}
---
+++ JAVA-CURSO/src/Testes/collection/M_generics/metodosGen/Util.java
@@ -0,0 +1,47 @@
+package Testes.collection.M_generics.metodosGen;
+
+public class Util {
+    public static void main(String[] args) {
+        String[] nomes = {"Seya", "Shiryu", "Shun", "Yoga", "Iki"};
+
+        System.out.println("== ANTES DA TROCA ==");
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        troca(nomes, 0, 2);
+
+
+        System.out.println("== DEPOIS DA TROCA ==");
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        System.out.println("------------------------------");
+
+        imprimirQqTipo(43);
+        imprimirQqTipo("Java é a linguagem mais braba que tem?");
+        imprimirQqTipo(true);
+
+        System.out.println("------------------------------");
+
+        System.out.println(menor("Banana", "Abacaxi"));
+        System.out.println(menor(3658, 14587));
+    }
+
+
+
+    public static <T> void troca(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static <T> void imprimirQqTipo(T item) {
+        System.out.println(item);
+    }
+
+    public static <T extends Comparable<T>> T menor(T a, T b) {
+        return a.compareTo(b) < 0 ? a : b;
+    }
+}
